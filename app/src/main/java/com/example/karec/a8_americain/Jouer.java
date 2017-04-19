d296a2062360494f143a771b8b3028c1781a1e6a
elementalisa <abcza@hotmail.fr>
2017-04-19 09:44:53 +0100

    Creation des vues graphiques

---
+++ app/src/main/java/com/example/karec/a8_americain/Jouer.java
@@ -0,0 +1,43 @@
+package com.example.karec.a8_americain;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Karec on 18/04/2017.
+ */
+
+public class Jouer extends Activity{
+    private Button listeViewParties;
+    private Button creerPartie;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jouer);
+
+        listeViewParties = (Button) findViewById(R.id.buttonRejoindrePartie);
+        listeViewParties.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), ListeViewParties.class);
+                startActivity(intent);
+            }
+        });
+
+        creerPartie = (Button) findViewById(R.id.buttonCreePartie);
+        creerPartie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), SalonPartie.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/karec/a8_americain/PartieSalon.java
@@ -0,0 +1,8 @@
+package com.example.karec.a8_americain;
+
+/**
+ * Created by Karec on 18/04/2017.
+ */
+
+public class PartieSalon {
+}
